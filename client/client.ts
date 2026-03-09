3d3828369f7b2aa9944d09235eb850e0cd736a10
Irma Grecia Saldaña Garcidueñas <gr_rocket@outlook.com>
2026-03-09 08:41:08 -0600

    Add program variable assignment in client.ts

--- client/client.ts
+++ client/client.ts
@@ -2,3 +2,4 @@
 console.log("My address:", pg.wallet.publicKey.toString());
 const balance = await pg.connection.getBalance(pg.wallet.publicKey);
 console.log(`My balance: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
+const program = pg.program;
